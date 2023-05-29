fc8deebc5ddfc681efb91c05458322f9cf1de45d
Conor <37616617+Conor-Ryan8@users.noreply.github.com>
2023-05-29 15:19:17 +0100

    Raspberry Pi + Python version

--- smart_heat.py
+++ smart_heat.py
@@ -1,30 +1,34 @@
+import RPi.GPIO as GPIO
 import Adafruit_DHT
 from rpi_rf import RFDevice
 import time
 
-GPIO.setwarnings(False)
-radio = RFDevice(14)
-radio.enable_tx()
-
-IDEAL = 29
+RADIO_PIN = 14
+RADIO_PULSE_LENGTH = 190
+RADIO_PROTOCOL = 1
 
 HEAT_ON = 5330227
 HEAT_OFF = 5330236
-PIN = 189
+SENSOR_PIN = 4
+SENSOR_TYPE = Adafruit_DHT.DHT22
+
+IDEAL = 21
+
+GPIO.setwarnings(False)
+radio = RFDevice(RADIO_PIN)
+radio.enable_tx()
 
 while True:
 
-  humid, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,4)
+  h, temp = Adafruit_DHT.read_retry(SENSOR_TYPE, SENSOR_PIN)
   print(str(round(temp, 1)) + "°C")
 
   if temp < IDEAL:
-    radio.tx_code(HEAT_ON, 1, PIN)
+    radio.tx_code(HEAT_ON, RADIO_PROTOCOL, RADIO_PULSE_LENGTH)
     print("Too Cold! Heat On!")
-    
   elif temp > IDEAL:
-    radio.tx_code(HEAT_OFF, 1, PIN)
+    radio.tx_code(HEAT_OFF, RADIO_PROTOCOL, RADIO_PULSE_LENGTH)
     print("Too Warm, Heat Off")
-    
   else:
     print("Temperature Perfect!")
 
